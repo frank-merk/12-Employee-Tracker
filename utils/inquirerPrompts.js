2d4622acaaba34f85c0137a00d6e2ad3cc2e7354
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-11 21:47:11 -0600

    Created Main Prompts

---
+++ utils/inquirerPrompts.js
@@ -0,0 +1,22 @@
+const prompts = [
+    {
+         name: "home",
+         type: "list",
+         message: "What would you like to do?",
+         choices: [
+
+            "Add a Department",
+           "Add a Role",
+           "Add an Employee",
+            "View Departments",
+            "View Roles",
+           "View Employees",
+           "Update an Employee Role",
+        
+         ]
+   },
+ ]
+ 
+         
+      
+ module.exports = prompts;
