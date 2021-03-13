4a0cc4d84c57e461cfb6668ae008effc5094de3f
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-13 09:21:28 -0600

    Attempted to write a function to add employees

--- utils/functions.js
+++ utils/functions.js
@@ -37,7 +37,7 @@ function begin() {
 }
 
 function addDepartment(){
-    
+
    
 }
 
@@ -51,7 +51,107 @@ function addRole(){
 }
 
 function addEmployee() {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: "Enter the employee's first name.",
 
+            },
+
+            {
+                type: 'input',
+                name: 'lastName',
+                message: "Enter the employee's last name.",
+
+            },
+
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is the employees role?",
+                choices: ['Lead Actor', 'Lead Actress', 'Lead Hair and Makeup', 'Junior VFX Designer', 'Best Boy', 'Accountant', 'Legal Analyst']
+
+            },
+
+            {
+                type: 'list',
+                name: 'boss',
+                message: "Who is the employee's manager?",
+                choices: ['Joel Coen', 'Jon Moore', 'Dan Theman', 'none']
+
+            },
+
+        ]).then(function (response) {
+
+            //change role.response to role id
+            let role = response.role;
+            switch(role){
+            case "Lead Actor":
+                role = 6;
+                break;
+            case "Lead Actress":
+                role = 7;
+                break;
+            case "Lead Hair and Makeup":
+                role = 2;
+                break;
+            case "Junior VFX Designer":
+                role = 3;
+                break;
+            case "Best Boy":
+                role = 5;
+                break;
+            case "Accountant":
+                role = 11;
+                break;
+            case "Legal Analyst":
+                role = 10;
+                break;
+            default:
+                console.log("Good bye!");
+            }
+
+            let manager = response.boss
+            if (manager == 'none') {
+                manager = null;
+                
+            } else if (manager == 'Joel Coen') {
+                manager = 1;
+            } else if (manager == 'Jon Moore') {
+                manager = 2;
+            } else {
+                manager = 3;
+            }
+            console.log("Creating a new employee...\n");
+            //insert into SQL
+               connection.query(
+                "INSERT INTO employees SET ?",
+                {
+                  first_name: response.firstName,
+                  last_name: response.lastName,
+                  manager: manager
+                },
+                function(err, res) {
+                  if(err) throw err;
+                }
+                );
+                connection.query(
+                    "INSERT INTO roles SET ?",
+                    {
+                      title: role,
+                      
+                    },
+                    function(err, res) {
+                      if(err) throw err;
+                    }
+                  );
+                  
+                  
+        
+            begin();
+        })
 }
 
 function viewDepartment() {
